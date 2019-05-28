177acfd9c07f65bedc7c6f75ac0a1cf650cb0da6
github username <github email address>
2019-05-28 15:13:10 -0400

    round_2

--- lib/song.rb
+++ lib/song.rb
@@ -1,27 +1,47 @@
 require 'pry'
+
 class Song
-    attr_accessor :artist, :genre, :name
-    
-    @@artists = []
     @@count = 0
+    @@artists = []
     @@genres = []
 
+    attr_accessor :name, :artist,:genre
 
-    def initialize(name, artist, genre)
+    def initialize(name,artist,genre)
+        @name = name
         @artist = artist
         @genre = genre
-        @name = name
         @@count +=1
         @@artists << artist
+        @@genres << genre
     end
 
-    def name
-        @name
-    end
-    
-    def artist
-        @artist
+    def self.count
+        @@count
     end
 
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        hash =Hash.new(0)
+        @@genres.each do |element|
+            hash[element] +=1
+        end
+        hash
+    end
+
+    def self.artist_count
+        hash =Hash.new(0)
+        @@artists.each do|element|
+            hash[element] +=1
+        end
+        hash
+    end
 
 end
